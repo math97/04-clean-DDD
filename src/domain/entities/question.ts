afb619bdbb684a49f428b79e9852e004254aafaf
Matheus Pessoal <matheuspessoal@Mac.lan>
2025-03-17 08:49:00 +0000

    feat(entity): improve entity with optional fields

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,29 @@
 import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { Optional } from "../../core/types/optional"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  authorId: string
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id)
+
+    return question
+  }
+}
