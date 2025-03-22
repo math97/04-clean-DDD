7cfbc131d813725996249171bd303d0ff32cee49
Matheus Pessoal <matheuspessoal@Mac.lan>
2025-03-22 17:47:39 +0000

    fix(answer-question): Fix answer-question usecase after changing the way to create a new Question

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,22 +1,25 @@
-import { Answer } from "../entities/answer"
-import { AnswersRepository } from "../repositories/answers-repository"
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string
-  questionId: string
+  instructorId: UniqueEntityID
+  questionId: UniqueEntityID
   content: string
 }
 
 export class AnswerQuestionUseCase {
-  constructor(
-    private answersRepository: AnswersRepository,
-  ) {}
+  constructor(private answersRepository: AnswersRepository) {}
 
-  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
       content,
       authorId: instructorId,
-      questionId, 
+      questionId,
     })
 
     await this.answersRepository.create(answer)
