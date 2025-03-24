eeaaef414186a3fbb6328945dbbc9b099433f2a3
Matheus Pessoal <matheuspessoal@Mac.lan>
2025-03-24 08:54:18 +0000

    feat(question): create search question by slug use-case

--- src/test/repositories/in-memory-questions-repository.ts
+++ src/test/repositories/in-memory-questions-repository.ts
@@ -3,7 +3,18 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public questions: Question[] = []
+
   async create(question: Question) {
     this.questions.push(question)
+  }
+
+  async findBySlug(slug: string) {
+    const question = this.questions.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
   }
 }
