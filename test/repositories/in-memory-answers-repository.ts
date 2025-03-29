077e50fe5489c240ceb858ff7ae6f4a1027ca556
Matheus Pessoal <matheuspessoal@Mac.lan>
2025-03-29 15:41:33 +0000

    feat(question): delete answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -6,4 +6,20 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async create(answer: Answer) {
     this.answers.push(answer)
   }
+
+  async findById(id: string) {
+    const answer = this.answers.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.answers.findIndex((item) => item.id === answer.id)
+
+    this.answers.splice(itemIndex, 1)
+  }
 }
