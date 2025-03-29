b84fbbeefd39b9da34da3f3eafd86e80b93f275e
Matheus Pessoal <matheuspessoal@Mac.lan>
2025-03-29 16:08:12 +0000

    feat(question): edit answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -3,8 +3,15 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public answers: Answer[] = []
+
   async create(answer: Answer) {
     this.answers.push(answer)
+  }
+
+  async save(answer: Answer) {
+    const itemIndex = this.answers.findIndex((item) => item.id === answer.id)
+
+    this.answers[itemIndex] = answer
   }
 
   async findById(id: string) {
