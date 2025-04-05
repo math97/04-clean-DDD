a452aa412f06a5987b0d9e507e154013503fd2e9
Matheus Albuquerque <math.albuquerque@outlook.com>
2025-04-06 00:42:05 +0100

    feat(notification): create read-notification use-case

--- src/domain/notification/application/use-cases/send-notification.spec.ts
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -1,5 +1,5 @@
-import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
 import { SendNotificationUseCase } from './send-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 let sut: SendNotificationUseCase
